36c26a0cba1da728323d3eca3daeb5583e89a0cc
Sumit saurav <117914094+sumitsaurav1@users.noreply.github.com>
2023-06-18 00:01:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const line = document.getElementById('line');
+let angle = 0;
+
+function rotateLine() {
+  line.style.transform = `rotate(${angle}deg)`;
+  angle += 1; // Adjust the rotation speed here
+  requestAnimationFrame(rotateLine);
+}
+
+rotateLine();
